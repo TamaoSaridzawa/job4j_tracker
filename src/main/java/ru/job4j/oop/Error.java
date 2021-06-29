c80ad77541988dbe2fcd4df15abc658ef34ff08c
TamaoSaridzawa <tamaosaridzawa9@gmail.com>
2021-06-29 22:57:08 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,33 @@
+package ru.job4j.oop;
+
+public class Error {
+
+    private boolean active;
+    private int status;
+    private String message;
+
+   public Error() {
+
+   }
+
+   public Error(boolean active, int status, String message) {
+       this.active = active;
+       this.status = status;
+       this.message = message;
+   }
+
+   public void printInfo(){
+       System.out.println("Активный : " + active);
+       System.out.println("Статус : " + status);
+       System.out.println("Сообщение : " + message);
+   }
+
+    public static void main(String[] args) {
+        Error error1 = new Error();
+        Error error2 = new Error(true, 1, "Авторизация прошла успешно!");
+        Error error3 = new Error(false, 2, "Ошибка авторизации");
+        error1.printInfo();
+        error2.printInfo();
+        error3.printInfo();
+    }
+}
