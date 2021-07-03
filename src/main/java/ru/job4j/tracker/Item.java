7b46652726cab70e0725aa8adf5dd880a5296878
TamaoSaridzawa <tamaosaridzawa9@gmail.com>
2021-07-04 00:56:39 +0300

    6. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,15 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+
 public class Item {
     private int id;
     private String name;
+    private LocalDateTime created = LocalDateTime.now();
+
+    public Item( LocalDateTime created) {
+        this.created = created;
+    }
 
     public int getId() {
         return id;
@@ -19,6 +26,10 @@ public class Item {
     public Item(int id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
     }
 
     public void setId(int id) {
