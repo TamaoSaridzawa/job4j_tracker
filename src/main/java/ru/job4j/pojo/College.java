1ba78d3b198989ca2ddb7efe20784cdef0b34760
TamaoSaridzawa <tamaosaridzawa9@gmail.com>
2021-07-04 23:50:03 +0300

    2. Модель данных. [#174683]

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,23 @@
+package ru.job4j.pojo;
+
+import java.time.MonthDay;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class College {
+
+    public static void main(String[] args) {
+        Student student = new Student();
+        Calendar calendar = new GregorianCalendar();
+        calendar.set(Calendar.YEAR, 2021);
+        calendar.set(Calendar.MONTH, 5);
+        calendar.set(Calendar.DAY_OF_MONTH, 9);
+        student.setFamily("Ryzhov ");
+        student.setName("Danila ");
+        student.setSurname("Yurievich ");
+        student.setGroup("intern ");
+        student.setEntrance(calendar.getTime());
+        System.out.println(student.getFamily() + student.getName() + student.getSurname() + student.getGroup() + student.getEntrance());
+    }
+}
---
+++ src/main/java/ru/job4j/pojo/Student.java
@@ -0,0 +1,52 @@
+package ru.job4j.pojo;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class Student {
+    private String family;
+    private String name;
+    private String surname;
+    private String group;
+    private Date entrance;
+
+    public String getFamily() {
+        return family;
+    }
+
+    public void setFamily(String family) {
+        this.family = family;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public Date getEntrance() {
+        return entrance;
+    }
+
+    public void setEntrance(Date entrance) {
+        this.entrance = entrance;
+    }
+}
