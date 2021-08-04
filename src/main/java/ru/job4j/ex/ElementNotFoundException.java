809048ea069e8e217d503a4cdfdf541a998361f8
TamaoSaridzawa <tamaosaridzawa9@gmail.com>
2021-08-04 22:39:12 +0300

    0.4. Пользовательские исключения. [#219367 #184584]

--- src/main/java/ru/job4j/ex/ElementNotFoundException.java
+++ src/main/java/ru/job4j/ex/ElementNotFoundException.java
@@ -1,13 +1,10 @@
 package ru.job4j.ex;
 
 public class ElementNotFoundException extends Exception{
-    public static void main(String[] args) {
-        String[] array = {"Alex", "Garik", "Rex", "Gav", "Tyapa"};
-        try {
-            System.out.println("Элемент с индексом №2 существует в массиве, его имя - " +array[FindEl.indexOf(array, "3")] );
-        } catch (ElementNotFoundException e) {
-            e.printStackTrace();
-        }
 
+    public ElementNotFoundException(String message) {
+        super(message);
     }
+
+
 }
--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -2,16 +2,20 @@ package ru.job4j.ex;
 
 public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
-        int keyInt = Integer.parseInt(key);
-        if (keyInt >= value.length || keyInt < 0) {
-            throw  new ElementNotFoundException();
-        } else {
             for (int i = 0; i < value.length; i++) {
-                if (keyInt == i) {
+                if (key.equals(String.valueOf(i))) {
                     return i;
                 }
             }
+            throw new ElementNotFoundException("Ошибка ввода данных");
+    }
+
+    public static void main(String[] args) throws ElementNotFoundException{
+        String[] array = {"Alex", "Garik", "Rex", "Gav", "Tyapa"};
+        try {
+            System.out.println("Элемент с индексом №3 существует в массиве, его имя - " + array[FindEl.indexOf(array, "3")] );
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
         }
-        return -1;
     }
 }
