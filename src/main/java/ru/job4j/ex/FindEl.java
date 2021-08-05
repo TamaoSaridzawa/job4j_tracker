94cc6aa578b9e836c488480049f4185e4535a1b7
TamaoSaridzawa <tamaosaridzawa9@gmail.com>
2021-08-05 22:40:53 +0300

    0.4. Пользовательские исключения. [#219367 #184584]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -3,7 +3,7 @@ package ru.job4j.ex;
 public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
             for (int i = 0; i < value.length; i++) {
-                if (key.equals(String.valueOf(i))) {
+                if (key.equals(value[i])) {
                     return i;
                 }
             }
@@ -13,7 +13,7 @@ public class FindEl {
     public static void main(String[] args) throws ElementNotFoundException{
         String[] array = {"Alex", "Garik", "Rex", "Gav", "Tyapa"};
         try {
-            System.out.println("Элемент с индексом №3 существует в массиве, его имя - " + array[FindEl.indexOf(array, "3")] );
+            System.out.println("Индекс элемента найден : " + FindEl.indexOf(array, "Gav") );
         } catch (ElementNotFoundException e) {
             e.printStackTrace();
         }
