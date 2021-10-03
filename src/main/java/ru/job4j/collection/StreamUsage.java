d8e30d0e7cac2ec6efe97662ac3e2f4f75d8dbbc
TamaoSaridzawa <tamaosaridzawa9@gmail.com>
2021-10-03 08:04:04 +0300

    0. Stream API [#58166 #211361]

--- src/main/java/ru/job4j/collection/StreamUsage.java
+++ src/main/java/ru/job4j/collection/StreamUsage.java
@@ -7,7 +7,7 @@ public class StreamUsage {
     public static void main(String[] args) {
         List<Integer> list = List.of(4, 0, -1, -4, 3, -2, 8, -7);
         List<Integer> stream = list.stream()
-                .filter(in -> in >= 0)
+                .filter(in -> in > 0)
                 .collect(Collectors.toList());
         stream.forEach(System.out::println);
     }
